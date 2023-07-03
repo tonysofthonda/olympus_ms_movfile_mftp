ac8ab2fabfa109ebdbe484e5dadf58616d147907
rlcazares <rlcazares@latam.stefanini.com>
2023-07-03 11:48:20 -0600

    Squashed commit of the following:
    commit cb3de47c84f9e951afc34b9f7760093b7cfc4f54
    Author: rlcazares <rlcazares@latam.stefanini.com>
    Date:   Mon Jul 3 11:41:54 2023 -0600
    
        correción escritura en carpeta nueva
    
    commit a751dd7a8cb821ca2b175986532c43e25deaa052
    Author: rlcazares <rlcazares@latam.stefanini.com>
    Date:   Mon Jul 3 11:40:43 2023 -0600
    
        Corrección properties nueva carpeta, correción escritura en carpeta nueva
    
    commit 9ba4425be03c0387849ddf8cac6f968644334d0b
    Author: rlcazares <rlcazares@latam.stefanini.com>
    Date:   Thu Jun 29 10:18:49 2023 -0600
    
        Correción conexión SFTP, corrección propoerties para Test y QA, optimización cierre de conexiones
    
    commit 9791ce8555076f12c1d3bbe99cecc9107087adf3
    Author: vzpintor <ovpintor@stefanini.com>
    Date:   Thu Jun 15 16:29:08 2023 -0600
    
        fixed variables on profile env
    
    commit 03116f80cef5a9c456d6b6576ec08fe019e6a938
    Author: vzpintor <ovpintor@stefanini.com>
    Date:   Thu Jun 15 11:25:15 2023 -0600
    
        configured profiles for environments

--- src/main/java/com/honda/olympus/ms/movfile_mftp/client/MftpClient.java
+++ src/main/java/com/honda/olympus/ms/movfile_mftp/client/MftpClient.java
@@ -2,7 +2,9 @@ package com.honda.olympus.ms.movfile_mftp.client;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -12,137 +14,176 @@ import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPReply;
 
 import com.honda.olympus.ms.movfile_mftp.util.FileUtil;
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelSftp;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+import com.jcraft.jsch.SftpException;
 
 import lombok.extern.slf4j.Slf4j;
-
+import net.schmizz.sshj.SSHClient;
+import net.schmizz.sshj.sftp.SFTPClient;
+import net.schmizz.sshj.transport.verification.PromiscuousVerifier;
 
 @Slf4j
-public class MftpClient 
-{ 
-	
-	private FTPClient ftp;
+public class MftpClient {
+
+	//private FTPClient ftp;
 	private MftpConfig config;
-	
+
 	private String fileName;
-	private String input;  
+	private String input;
 	private String output;
-	
-	private boolean remoteDirFound; 
-	
-	
+
+	private Channel channel = null;
+	private ChannelSftp channelSftp = null;
+	private Session session = null;
+
+	private boolean remoteDirFound;
+
 	public MftpClient(MftpConfig config, String fileName) {
 		this.config = config;
 		this.fileName = fileName;
-		this.input = FileUtil.fixSlashes( FileUtil.concat(config.getSource(), fileName) );
-		this.output = FileUtil.fixSlashes( FileUtil.concat(config.getOutbound(), fileName) );
+		this.input = FileUtil.fixSlashes(FileUtil.concat(config.getSource(), fileName));
+		this.output = FileUtil.fixSlashes(FileUtil.concat(config.getOutbound(), fileName));
 	}
-	
-	
+
 	// setter method added for testing purposes only
-	
+
 	public void setFileName(String fileName) {
 		this.fileName = fileName;
-		this.input = FileUtil.fixSlashes( FileUtil.concat(config.getSource(), fileName) );
-		this.output = FileUtil.fixSlashes( FileUtil.concat(config.getOutbound(), fileName) );
+		this.input = FileUtil.fixSlashes(FileUtil.concat(config.getSource(), fileName));
+		this.output = FileUtil.fixSlashes(FileUtil.concat(config.getOutbound(), fileName));
 	}
-	
+
 	
 	public boolean open() {
-		try {
-			ftp = new FTPClient();
-	        ftp.addProtocolCommandListener(new PrintCommandListener(new PrintWriter(System.out)));
-	        
-	        ftp.connect(config.getHost(), config.getPort());
-	        ftp.login(config.getUser(), config.getPass());
-	        
-	        ftp.enterLocalPassiveMode(); 
-	        ftp.setFileType(FTP.BINARY_FILE_TYPE);
-	        
-	        int reply = ftp.getReplyCode();
-	        if (!FTPReply.isPositiveCompletion(reply)) 
-	        {
-	        	ftp.disconnect();
-	        	log.error("### Can't connect to the ftp server");
-	        	return false;
-	        }
-	        return true;
-		}
-		catch (IOException ioe) {
-			log.error("### Error found while connecting to ftp server", ioe);
-			return false;
-		}
-    }
 	
+		try {
+			/*
+			String pass = config.getPass();
+			JSch jsch = new JSch();
+			this.session = jsch.getSession(config.getUser(),config.getHost(), config.getPort());
+			this.session.setConfig("StrictHostKeyChecking", "no");
+			this.session.setPassword(pass);
+			this.session.connect();
+			log.debug("Connection established.");
+			log.debug("Creating SFTP Channel.");
+
+			this.channel = this.session.openChannel("sftp");
+			this.channel.connect();*/
+
+			return true;
+		} catch (Exception e4) {
+			log.error("### Error found while connecting to ftp server", e4);
+			return false;
+
+		}
+	}
+	
+	private SSHClient setupSshj() throws IOException {
+	    SSHClient client = new SSHClient();
+	    client.addHostKeyVerifier(new PromiscuousVerifier());
+	    client.connect(config.getHost(),config.getPort());
+	    client.authPassword(config.getUser(),config.getPass());
+	    return client;
+	}
+
+	
+	public void whenUploadFileUsingSshj_thenSuccess() throws IOException {
+	    SSHClient sshClient = setupSshj();
+	    SFTPClient sftpClient = sshClient.newSFTPClient();
+	 
+	    sftpClient.put(this.input,this.output);
+	 
+	    sftpClient.close();
+	    sshClient.disconnect();
+	    log.info("Monitor:: SFTP channel & session disconnected");
+	}
 	
 	public boolean localFileExists() {
+		
 		try {
-			Path path = Paths.get(input);
+			Path path = Paths.get(this.input);
 			if (!path.toFile().exists()) {
 				log.error("### Can't find local file '{}'", fileName);
 				return false;
 			}
 			return true;
-		}
-		catch (SecurityException ioe) {
+		} catch (SecurityException ioe) {
 			log.error("### Error found while searching local file '{}'", fileName, ioe);
 			return false;
 		}
+		
+		
 	}
-	
-	
+
 	public boolean uploadFile() {
-		FileInputStream fis = null;
+		
 		try {
-			findRemoteDir();
-			
-			fis = new FileInputStream(input);
-		    if (!ftp.storeFile(output, fis)) 
-		    {
-		    	log.error("### Can't upload file '{}'", fileName);
-		    	fis.close();
-				return false;
-		    }
-			fis.close();
-		    return true;
+			whenUploadFileUsingSshj_thenSuccess();	
+			return true;
 		}
+		
+		/*
+		//findRemoteDir();
+		Path path = Paths.get(this.input);
+		
+		try (InputStream inputStream = Files.newInputStream(path)){
+			
+			this.channelSftp = (ChannelSftp) this.channel;	
+			this.channelSftp.put(this.input,this.output,null,1);
+			//this.channelSftp.put( inputStream,this.output,0);
+			//this.channelSftp.rename(this.input, fileName);
+			
+			this.channelSftp.exit();
+			return true;
+		}*/
 		catch (IOException ioe) {
-			log.error("### Error found while uploading file '{}'", fileName, ioe);
-			if (fis != null) {
-				try { fis.close(); } catch (IOException e) { }
-			}
+			ioe.printStackTrace();
+			log.error("### Can't upload file '{}' due to: {}", fileName,ioe.getLocalizedMessage());
 			return false;
 		}
+		
+		
 	}
-	
-	
+
 	private void findRemoteDir() throws IOException {
-		remoteDirFound = ftp.changeWorkingDirectory(config.getOutbound());
-		if (!remoteDirFound) {
-			log.error("### Directory '{}' doesn't exist in ftp server !", config.getOutbound());
+	
+		try {
+			this.channelSftp = (ChannelSftp) this.channel;
+			this.channelSftp.get(this.input,this.output);
+			
+		}
+		catch (SftpException ioe) {
+			log.error("### Can't find remote file '{}'", fileName);
+			
 		}
 	}
-	
-	
+
 	public boolean remoteDirFound() {
 		return this.remoteDirFound;
 	}
-	
-	
+
 	public boolean deleteLocalFile() {
 		return FileUtil.removeFile(input);
 	}
-	
-	
+
 	public boolean close() {
+	
 		try {
-			ftp.logout();
-			ftp.disconnect();
+			/*
+			this.channelSftp.disconnect();
+			this.channel.disconnect();
+			this.session.disconnect();
+			*/
 			return true;
 		}
-		catch (IOException ioe) {
+		catch (Exception ioe) {
 			log.error("### Error found while closing connection to ftp server", ioe);
 			return false;
 		}
-    }
-	
+	}
+
 }
